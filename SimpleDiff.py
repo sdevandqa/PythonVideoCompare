f3226e8fe11b294bf67c035e620eb580c002a2a7
sdevandqa <sdevandqa@outlook.com>
2024-06-22 21:07:29 +0100

    cleanup

--- SimpleDiff.py
+++ SimpleDiff.py
@@ -4,22 +4,29 @@ from moviepy.editor import *
 
 root = tk.Tk()
 root.withdraw()
-file_path_1 = filedialog.askopenfilename()
-file_path_2 = filedialog.askopenfilename()
 
-vid1 = VideoFileClip(file_path_1)
-vid2 = VideoFileClip(file_path_2)
+file_path_1 = filedialog.askopenfilename(filetypes=[("mp4 Files",".mp4"), ("Mov Files",".mov")])
+file_path_2 = filedialog.askopenfilename(filetypes=[("mp4 Files",".mp4"), ("Mov Files",".mov")])
 
-vid1 = vid1.margin(left=0, right=20, top=0, bottom=0)
+try:
+    vid1 = VideoFileClip(file_path_1)
+    vid1 = vid1.margin(left=0, right=20, top=0, bottom=0)
+    v1_txt_clip = TextClip(file_path_1, fontsize = 30, color = 'white') 
+    v1_txt_clip = v1_txt_clip.set_pos('bottom', 'left').set_duration(10) 
+    vid1 = CompositeVideoClip([vid1, v1_txt_clip]) 
+except IOError:
+    print ('No file specified for file 1.')
 
-v1_txt_clip = TextClip(file_path_1, fontsize = 30, color = 'white') 
-v1_txt_clip = v1_txt_clip.set_pos('bottom', 'left').set_duration(10) 
+try:
+    vid2 = VideoFileClip(file_path_2)
+    v2_txt_clip = TextClip(file_path_2, fontsize = 30, color = 'white') 
+    v2_txt_clip = v2_txt_clip.set_pos('bottom', 'left').set_duration(10) 
+    vid2 = CompositeVideoClip([vid2, v2_txt_clip]) 
+except IOError:
+    print ('No file specified for file 2.')
 
-v2_txt_clip = TextClip(file_path_2, fontsize = 30, color = 'white') 
-v2_txt_clip = v2_txt_clip.set_pos('bottom', 'left').set_duration(10) 
-
-vid1 = CompositeVideoClip([vid1, v1_txt_clip]) 
-vid2 = CompositeVideoClip([vid2, v2_txt_clip]) 
-
-combined = clips_array([[vid1, vid2]])
-combined.write_videofile('VideoDiff.mp4')
+try:
+    combined = clips_array([[vid1, vid2]])
+    combined.write_videofile('VideoDiff.mp4')
+except NameError:
+    print("Missing files. Reload script and try again.")
