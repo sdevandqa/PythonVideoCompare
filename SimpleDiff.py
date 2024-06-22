83eec8867ae7e7d8fbbf358eeb80201329ece5f7
sdevandqa <sdevandqa@outlook.com>
2024-06-22 17:56:57 +0100

    init commit

---
+++ SimpleDiff.py
@@ -0,0 +1,25 @@
+import  tkinter as tk
+from tkinter import filedialog
+from moviepy.editor import *
+
+root = tk.Tk()
+root.withdraw()
+file_path_1 = filedialog.askopenfilename()
+file_path_2 = filedialog.askopenfilename()
+
+# Read files
+vid1 = VideoFileClip(file_path_1)
+vid2 = VideoFileClip(file_path_2)
+
+# Generate a text clip for both videos
+v1_txt_clip = TextClip(file_path_1, fontsize = 30, color = 'white') 
+v1_txt_clip = v1_txt_clip.set_pos('bottom', 'left').set_duration(10) 
+
+v2_txt_clip = TextClip(file_path_2, fontsize = 30, color = 'white') 
+v2_txt_clip = v2_txt_clip.set_pos('bottom', 'left').set_duration(10) 
+
+vid1 = CompositeVideoClip([vid1, v1_txt_clip]) 
+vid2 = CompositeVideoClip([vid2, v2_txt_clip]) 
+
+combined = clips_array([[vid1, vid2]])
+combined.write_videofile('VideoDiff.mp4')
